6af52e86947e8e9515a28d5bb7299a60cdc9db0e
Neil <ngarcia@soin.co.cr>
2016-11-07 11:54:32 -0600

    Initial Commit

---
+++ Sticky/Sticky.h
@@ -0,0 +1,19 @@
+//
+//  Sticky.h
+//  Sticky
+//
+//  Created by Neil on 7/11/16.
+//  Copyright © 2016 kartel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sticky.
+FOUNDATION_EXPORT double StickyVersionNumber;
+
+//! Project version string for Sticky.
+FOUNDATION_EXPORT const unsigned char StickyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sticky/PublicHeader.h>
+
+
